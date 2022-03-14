6a5ea833f2db3e730b081164a576b7ec450594db
sabahb3 <sabahbaara4@gmail.com>
2022-03-14 10:27:33 +0200

    Implement add method to add zero, one or two numbers

---
+++ StringCalculatorKata/src/StringCalculator/StringCalculator.cs
@@ -0,0 +1,49 @@
+namespace StringCalculator;
+
+public class StringCalculator
+{
+    public int add(string numbers)
+    {
+        var digits = numbers.Split(',');
+        var sum = 0;
+        switch (digits.Length)
+        {
+            case 1:
+                sum = AddSingleNumber(digits[0]);
+                break;
+            case 2:
+                sum = AddMoreThanOneNumber(digits);
+                break;
+        }
+
+        return sum;
+    }
+
+    private int AddSingleNumber(string number)
+    {
+        if (string.IsNullOrWhiteSpace(number)) return 0;
+        else
+        {
+            int.TryParse(number, out var num);
+            return num;
+        }
+    }
+
+    private int AddMoreThanOneNumber(string[] numbers)
+    {
+        int sum = 0;
+        foreach (var number in numbers)
+        {
+            if(string.IsNullOrWhiteSpace(number))continue;
+            if (int.TryParse(number, out var num))
+            {
+                sum += num;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return sum;
+    }
+}
