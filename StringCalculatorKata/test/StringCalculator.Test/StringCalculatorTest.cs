3170088e783e4bf19bb6a19c1fd61ddbc4966cab
sabahb3 <sabahbaara4@gmail.com>
2022-03-14 11:12:58 +0200

    Adding test case to check the result when the add method took a string with single number

--- StringCalculatorKata/test/StringCalculator.Test/StringCalculatorTest.cs
+++ StringCalculatorKata/test/StringCalculator.Test/StringCalculatorTest.cs
@@ -18,4 +18,18 @@ public class StringCalculatorTest
         Assert.Equal(0,result);
 
     }
+    [Fact]
+    public void Should_Return_The_Same_Number_When_Adding_String_With_Single_Number()
+    {
+        //Arrange
+        StringCalculator stringCalculator = new StringCalculator();
+        string numbers = "1";
+    
+        // Act
+        int result = stringCalculator.add(numbers);
+    
+        // Assert
+        Assert.Equal(1,result);
+
+    }
 }
